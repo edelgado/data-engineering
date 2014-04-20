9cd070767617827fe63ee2ef38b083ac6a8cb3f9
Enrique Delgado <edelgado@digium.com>
2014-04-20 13:24:26 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Livingsocial::Application.config.secret_key_base = '9c7147be37fa5e0c21655885f6757577227569ba9d78d2aca06b33a9a489d705a644e5a75e5f9b4fc68045f0deca7384103c7bc620d7102752d4b5a0b2bb845a'
