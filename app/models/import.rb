ac6267bf3db01c6e4db4b9d8019a03af65ccb9a2
Enrique Delgado <edelgado@digium.com>
2014-04-25 07:36:27 -0500

    Setup approach for parsing file. Next, use the models we have setup to do the calculations.

--- app/models/import.rb
+++ app/models/import.rb
@@ -1,4 +1,18 @@
 class Import < ActiveRecord::Base
   validates :file, presence: true
   mount_uploader :file, FileUploader
+
+  def process_file
+    require 'csv'
+    # Parse the associated file into an array of arrays:
+    CSV.foreach(self.file.path, {col_sep: "\t"}) do |row|
+      # Use the row here.
+      # Look at each value and create matching clients, merchants and transactions.
+      # Then use model methods to get the total revenue from different models. For example,
+      # we can look at the total revenue, by doing a static method on the Transaction model,
+      # like so: Transaction.total_revenue(), or just the last uploaded file by using an
+      # instance method like an_import.total_revenue() (on the Import model).
+      # Similar things can be done with the clients and merchants.
+    end
+  end
 end
