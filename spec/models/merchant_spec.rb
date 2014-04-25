588a65476b4df85f2ee8b9a2fa437b537431e566
Enrique Delgado <edelgado@digium.com>
2014-04-25 02:47:34 -0500

    Merchant RSpec model tests.

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,6 +1,23 @@
 require 'spec_helper'
 
 describe Merchant do
-  pending "should not allow more than one metchant with the same name"
-  pending "should have many transactions"
+
+  it "should have a valid factory" do
+    FactoryGirl.build(:merchant).should be_valid
+  end
+
+  it "should not allow more than one merchant with the same name" do
+    FactoryGirl.create(:merchant, name:'Acme Co.').should be_valid
+    FactoryGirl.build(:merchant, name:'Acme Co.').should_not be_valid
+  end
+
+  it "should have many transactions" do
+    merchant = FactoryGirl.create(:merchant)
+    10.times do 
+      FactoryGirl.create(:transaction, merchant: merchant)  
+    end
+    #binding.pry
+    expect(merchant.transactions.length).to eq(10)
+  end
+  
 end
