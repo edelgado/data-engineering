5d212fca70863d93b664637f3c2361351ae2e06f
Enrique Delgado <edelgado@digium.com>
2014-04-27 16:20:37 -0500

    Ability to reset the DB.

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -6,11 +6,23 @@ class ImportsController < ApplicationController
   def parse
     @file_upload = Import.new(import_params['import'])
     if @file_upload.save
-      flash[:notice] = 'Cool'
       @file_upload.process_file
+      @total_gross_revenue = Transaction.total_gross_revenue(@file_upload.id)
+      @cumulative_total_gross_revenue = Transaction.total_gross_revenue
+      @clients = Client.all.to_a
+      @merchants = Merchant.all.to_a
     else
       render action: 'upload'
     end
+  end
+
+  def start_over
+    Client.destroy_all
+    Merchant.destroy_all
+    Transaction.destroy_all
+    Import.destroy_all
+    flash['warning'] = "The DB has been cleared."
+    redirect_to action: 'upload'
   end
 
   private
