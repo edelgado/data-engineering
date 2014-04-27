d202503aab2ab0e8894f933d115a4f6a9b7d3b8a
Enrique Delgado <edelgado@digium.com>
2014-04-27 04:09:33 -0500

    Parsing through file and added a class method to calculate the total gross revenue.

--- app/models/import.rb
+++ app/models/import.rb
@@ -5,7 +5,7 @@ class Import < ActiveRecord::Base
   def process_file
     require 'csv'
     # Parse the associated file into an array of arrays:
-    CSV.foreach(self.file.path, {col_sep: "\t"}) do |row|
+    CSV.foreach(self.file.path, {col_sep: "\t", headers: :first_row, return_headers: false}) do |row|
       # Use the row here.
       # Look at each value and create matching clients, merchants and transactions.
       # Then use model methods to get the total revenue from different models. For example,
@@ -13,6 +13,28 @@ class Import < ActiveRecord::Base
       # like so: Transaction.total_revenue(), or just the last uploaded file by using an
       # instance method like an_import.total_revenue() (on the Import model).
       # Similar things can be done with the clients and merchants.
+      binding.pry
+
+      # Create the transaction record, creating merchants and clients as necessary:
+      a_transaction = Transaction.new(
+        item: row["item description"],
+        price: row["item price"],
+        quantity: row["purchase count"],
+        merchant: Merchant.find_or_initialize_by name: row["merchant name"],
+        client: Client.find_or_initialize_by name: row["client name"]
+      )
+
+      # Update the merchant address, if necessary:
+      if a_transaction.merchant.address != row["merchant_address"]
+        a_transaction.merchant.address = row["merchant_address"]
+      end
+
+      # Save the transaction and any new associated models:
+      begin
+      a_transaction.save!
+      rescue Exception => e
+        binding.pry
+      end      
     end
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,13 @@
 class Transaction < ActiveRecord::Base
   belongs_to :merchant
   belongs_to :client
+
+  # Class method that calculates the total gross revenue represented by all trasactions in the DB
+  def self.total_gross_revenue
+    total = 0;
+    self.all.each do |t|
+      total += t.quantity * t.price
+    end
+    return total.round(2)
+  end
 end
